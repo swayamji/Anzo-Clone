96face145b9d21fb4ade5958c6516a24ba63c294
swayamji <aroraswayam992@gmail.com>
2024-11-16 11:44:04 +0530

    changes

--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -30,7 +30,7 @@ const Page1 = () => {
 
   return (
     <div 
-        onMouseMove={(e) => mouseMoving(e)} className='h-screen max-md:h-[200vh] bg-white p-4 flex flex-col'>
+        onMouseMove={(e) => mouseMoving(e)} className='h-screen max-md:h-[200vh] bg-white p-4 max-md:p-1 flex flex-col'>
 
         <div id='ppp' className='py-22 relative text-white h-full rounded-[50px] bg-center bg-cover  w-full 
         max-lg:bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_962,h_971,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)] bg-[url(https://static.wixstatic.com/media/f1c650_678c0ceab5194893872c60fa3be4bcdc~mv2.jpg/v1/fill/w_1865,h_953,fp_0.69_0.64,q_85,usm_0.66_1.00_0.01,enc_auto/ANZO.jpg)]'>
--- src/pages/Page4.jsx
+++ src/pages/Page4.jsx
@@ -17,25 +17,34 @@ const Page4 = () => {
         scrollTrigger: {
           trigger: '.bH1',
           markers: false,
-          start: "top 40%",    // Start animation when top of .section4 is 80% down the viewport
+          start: "top 50%",    // Start animation when top of .section4 is 80% down the viewport
           end: "top 30%",      // End animation when top of .section4 is 40% down the viewport
           scrub: 1,
           once:true
       }
-    })
+    }),
+    gsap.to('.rotatee', {
+      rotate: 120,
+      duration: 14,
+      repeat: -1,
+      ease: 'linear',
+      transformOrigin: 'center', // Optional, ensures rotation around the center
+  });
+  
   }
  })
 
   return (
-    <div className='section4 bg-white max-md:h-[70vw] h-screen p-5 relative'>
-      <div className='h-[49vw] w-full rounded-[50px] max-md:rounded-[30px] bg-[url(https://static.wixstatic.com/media/f1c650_1baf8f9dcad444669565ec73ee697a92~mv2.jpg/v1/fill/w_1865,h_953,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Background%20Anzo_Studio.jpg)]'>
+    <div className='section4 bg-white max-md:h-[70vw] h-screen p-5 relative overflow-hidden '>
+      <div className='rotation h-[49vw] max-md:h-[60vw] w-full rounded-[50px] max-md:rounded-[30px] bg-[url(https://static.wixstatic.com/media/f1c650_1baf8f9dcad444669565ec73ee697a92~mv2.jpg/v1/fill/w_1865,h_953,al_c,q_85,usm_0.66_1.00_0.01,enc_auto/Background%20Anzo_Studio.jpg)]'>
+      <img className='rotatee max-md:top-5 w-[40vw] opacity-90 absolute right-10' src="src\assets\11383065.png" alt="" />
 
-        <div className='left text-white font-[anzo5] text-[20vw] leading-[16vw] p-[4.5vw] tracking-wide'>
+        <div className='left text-white font-[anzo5] text-[20vw] leading-[16vw] p-[4.5vw] tracking-wide max-md:text-[22vw] max-md:leading-[20vw]'>
           <h1 className='text-[#9B9B9B]'>WHAT</h1>
           <h1 className='bH1'>I DO</h1>
         </div>
-
-        <div className='bottom text-[1.3vw] text-white pl-[4vw] leading-[1.75vw]'>
+          
+        <div className='bottom text-[1.3vw] text-white absolute bottom-14 left-8 max-md:bottom-5 max-md:left-4 max-md:text-[1.7vw] leading-[1.75vw]'>
           <h4 className='font-[anzo4]'>I DESIGN AND DEVELOP</h4>
           <h4 className='font-[anzo3] text-gray-400'>MODERN, IMPACTFUL AND LUXURIOUS</h4>
           <h5 className='font-[anzo4]'>websites that seamlessly bridge your goals with needs of your clients</h5>
