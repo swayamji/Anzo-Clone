0d85d0cf03aec3680e632e1a6ea877147072e6d1
swayamji <aroraswayam992@gmail.com>
2024-11-17 12:01:28 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
-import React from 'react'
-import { Header } from './Components/Header'
-import Page1 from './pages/Page1'
-import Page2 from './pages/Page2'
-import Page3 from './pages/Page3'
-import Page4 from './pages/page4'
-import Page5 from './pages/Page5'
-import Page6 from './pages/Page6'
-import Page7 from './pages/Page7'
-
+import React from 'react';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Header } from './Components/Header';
+import Page1 from './pages/Page1';
+import Page2 from './pages/Page2';
+import Page3 from './pages/Page3';
+import Page4 from './pages/Page4';
+import Page5 from './pages/Page5';
+import Page6 from './pages/Page6';
+import Page7 from './pages/Page7';
 
 const App = () => {
   return (
-    <>
-    <Header/>
-    <Page1/>
-    <Page2/>
-    <Page3/>
-    <Page4/>
-    <Page5/>
-    <Page6/>
-    <Page7/>
-    </>
-  )
-}
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Page1 />} />
+        <Route path="/page2" element={<Page2 />} />
+        <Route path="/page3" element={<Page3 />} />
+        <Route path="/page4" element={<Page4 />} />
+        <Route path="/page5" element={<Page5 />} />
+        <Route path="/page6" element={<Page6 />} />
+        <Route path="/page7" element={<Page7 />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
