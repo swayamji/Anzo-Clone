b2e03becfe52a2a57e6432fea53d14a0c8e18ae1
swayamji <aroraswayam992@gmail.com>
2024-11-17 18:37:12 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/Anzo-Clone-/'  // This should match the repo name
+  base: '/Anzo-Clone/'  // This should match the repo name
 })
