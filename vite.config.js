877aa1d300585bb22bccbd3d42afd63a8687e4ec
swayamji <aroraswayam992@gmail.com>
2024-11-18 15:48:33 +0530

    Chnage

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Anzo-Clone/', // Replace 'Your-Repo-Name' with your GitHub repo name
+  base: './', // Replace 'Your-Repo-Name' with your GitHub repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
